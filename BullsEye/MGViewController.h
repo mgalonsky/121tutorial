fe092eb12371b2013fd9bf3195cdacec14fdeccc
Melissa Galonsky <mgalonsky@cs.hmc.edu>
2014-09-03 23:18:53 -0700

    Initial Commit

---
+++ BullsEye/MGViewController.h
@@ -0,0 +1,13 @@
+//
+//  MGViewController.h
+//  BullsEye
+//
+//  Created by Melissa Galonsky on 9/3/14.
+//  Copyright (c) 2014 Melissa Galonsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MGViewController : UIViewController
+
+@end
---
+++ BullsEye/MGViewController.m
@@ -0,0 +1,29 @@
+//
+//  MGViewController.m
+//  BullsEye
+//
+//  Created by Melissa Galonsky on 9/3/14.
+//  Copyright (c) 2014 Melissa Galonsky. All rights reserved.
+//
+
+#import "MGViewController.h"
+
+@interface MGViewController ()
+
+@end
+
+@implementation MGViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
