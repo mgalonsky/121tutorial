a101f2a81f902d0c1885f46905a9ecc7b45044b3
Melissa Galonsky <mgalonsky@cs.hmc.edu>
2014-09-09 22:49:13 -0700

    Finished through Page 132.

--- BullsEye/MGViewController.m
+++ BullsEye/MGViewController.m
@@ -104,8 +104,14 @@
 }
 
 - (IBAction)startOver {
+    CATransition *transition = [CATransition animation];
+    transition.type = kCATransitionFade;
+    transition.duration = 1;
+    transition.timingFunction = [CAMediaTimingFunction
+                                 functionWithName:kCAMediaTimingFunctionEaseOut];
     [self startNewGame];
     [self updateLabels];
+    [self.view.layer addAnimation:transition forKey:nil];
 }
 
 - (void)startNewGame {
